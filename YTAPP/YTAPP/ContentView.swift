744719c1ea2751d1d956663e08a8baef42d2ac03
GorbynoSitepu <61469586+byno723@users.noreply.github.com>
2022-05-06 12:54:32 +0700

    navigation bar

--- YTAPP/YTAPP/ContentView.swift
+++ YTAPP/YTAPP/ContentView.swift
@@ -9,13 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, earth!")
-            .padding()
+       Home()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+struct Home : View{
+    var body: some View{
+        NavigationView{
+            Text("Hello Ini Konten")
+                .navigationBarItems(leading: HStack{
+                    Button(action:{print("ok")}){
+                        Image("youtube").renderingMode(.original).resizable()
+                        .frame(width:90, height: 50)                    }
+                  
+                }, trailing:
+                    HStack{
+                    Button(action:{}){
+                        Image(systemName: "tray.fill")
+                            .foregroundColor(Color.secondary)
+                    }
+                    Button(action:{}){
+                        Image(systemName: "video.fill")
+                            .foregroundColor(Color.secondary)
+                    }
+                    Button(action:{}){
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(Color.secondary)
+                    }
+                    Button(action:{}){
+                        Image("profil").resizable().frame(width:30, height: 30).clipShape(Circle())
+                    }
+                    
+                }
+            )
+        }
     }
 }
