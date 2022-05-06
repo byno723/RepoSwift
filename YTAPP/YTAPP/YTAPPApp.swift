583ffeebf3693ecf493d1d8e30f9a1a8dad4cd48
GorbynoSitepu <61469586+byno723@users.noreply.github.com>
2022-05-06 12:11:53 +0700

    youtube repo

---
+++ YTAPP/YTAPP/YTAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  YTAPPApp.swift
+//  YTAPP
+//
+//  Created by gorbyno sitepu on 06/05/22.
+//
+
+import SwiftUI
+
+@main
+struct YTAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
