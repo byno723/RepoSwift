158f5046040039ac2c400fc62322d8a3c3bcafb9
gorbyno sitepu <gorbynositepuu@gmail.com>
2022-07-13 06:37:35 +0700

    update

---
+++ NewsApp/NewsApp/UIScreen/ProductScreen/ProductScreenViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ProductScreenViewModel.swift
+//  NewsApp
+//
+//  Created by gorbyno sitepu on 12/07/22.
+//
+
+import SwiftUI
+
+class ProductScreenViewModel: ObservableObject{
+    @Published var navigationSelection : String?
+}
